2961922af9f9f0d2ad184dcc57aadc5165b036c7
oriamrani <77613787+oriamrani@users.noreply.github.com>
2025-01-28 12:30:34 +0200

    Add files via upload

---
+++ pkge1/Main.java
@@ -0,0 +1,39 @@
+package pkge1;
+
+public class Main {
+    public static void main(String[] args) {
+
+        SecretCode[] secretCodeArr = { new MorseCode(), new RotCode(), new ReverseCode() };
+
+        for (SecretCode s : secretCodeArr) {
+            s.encode("Hello World");
+        }
+
+        for (SecretCode s : secretCodeArr) {
+            System.out.println("encoded " + s.getEncodedString());
+        }
+
+        for (SecretCode s : secretCodeArr) {
+            String tempSt = s.getEncodedString();
+            s.decode(tempSt);
+            System.out.println("decoded " + s.getDecodedString());
+        }
+
+        for (SecretCode s : secretCodeArr) {
+            String tempSt = s.getDecodedString();
+            s.encode(tempSt);
+            System.out.println("encoded " + s.getEncodedString());
+        }
+
+        for (SecretCode s : secretCodeArr) {
+            String tempSt = s.getEncodedString();
+            s.decode(tempSt);
+            System.out.println("decoded " + s.getDecodedString());
+        }
+    }
+
+
+
+    //System.out.println("Hello and welcome!");
+
+}
---
+++ pkge1/SecretCode.java
@@ -0,0 +1,18 @@
+package pkge1;
+
+public abstract class SecretCode {
+
+    protected String encodedString;
+    protected String decodedString;
+
+    public abstract void encode(String encodedString);
+    public abstract void decode(String decodedString);
+
+
+    public String getEncodedString() {
+        return encodedString;
+    }
+    public String getDecodedString() {
+        return decodedString;
+    }
+}
